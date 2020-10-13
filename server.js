fdea51b33580dbf06f7c4c7d1b49d0dc5f2e89d7
victor-delaunal <victor-delaunal@oulook.fr>
2020-10-13 13:20:21 +0200

    mise en place

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+// const ejs = require('ejs')
+// const bodyParser = require('body-parser')
+// const mongoose = require('mongoose')
+
+const app =  express()
+
+app.listen(3000, () => {console.log("démarrage serveur")})
